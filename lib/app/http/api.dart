de1c83e422367c459869bb61f8f75ca7ef2f8237
moxiaov587 <zouj1awang@163.com>
2021-12-12 17:16:12 +0800

    :hammer:add home related api

--- lib/app/http/api.dart
+++ lib/app/http/api.dart
@@ -7,4 +7,14 @@ class API {
 
   static String article({required int pageNum}) =>
       '/article/list/$pageNum/json';
+
+  static String square({required int pageNum}) =>
+      '/user_article/list/$pageNum/json';
+
+  static String qa({required int pageNum}) => '/wenda/list/$pageNum/json';
+
+  static String projectType = '/project/tree/json';
+
+  static String project({required int pageNum}) =>
+      '/project/list/$pageNum/json?cid=294';
 }
--- lib/app/http/wan_android_api.dart
+++ lib/app/http/wan_android_api.dart
@@ -1,15 +1,15 @@
 import 'package:dio/dio.dart';
 
-import '../../model/article_model.dart';
+import '../../model/models.dart';
 import 'api.dart';
 import 'http.dart';
 
 class WanAndroidAPI {
   const WanAndroidAPI._();
 
-  static Future<List<ArticleModel>> fetchArticles(
-    int pageNum, {
-    int? cid,
+  static Future<RefreshArticleListModel> fetchHomeArticles(
+    int pageNum,
+    int pageSize, {
     CancelToken? cancelToken,
   }) async {
     final Response<Map<String, dynamic>> response =
@@ -17,11 +17,55 @@ class WanAndroidAPI {
       API.article(
         pageNum: pageNum,
       ),
-      queryParameters: cid != null ? <String, dynamic>{'cid': cid} : null,
+      queryParameters: <String, dynamic>{'page_size': pageSize},
       cancelToken: cancelToken,
     );
-    return (response.data!['datas'] as List<dynamic>)
-        .map((dynamic e) => ArticleModel.fromJson(e as Map<String, dynamic>))
+    return RefreshArticleListModel.fromJson(response.data!);
+  }
+
+  static Future<RefreshArticleListModel> fetchSquareArticles(
+    int pageNum,
+    int pageSize, {
+    CancelToken? cancelToken,
+  }) async {
+    final Response<Map<String, dynamic>> response =
+        await HttpUtils.get<Map<String, dynamic>>(
+      API.square(
+        pageNum: pageNum,
+      ),
+      queryParameters: <String, dynamic>{'page_size': pageSize},
+      cancelToken: cancelToken,
+    );
+    return RefreshArticleListModel.fromJson(response.data!);
+  }
+
+  static Future<RefreshArticleListModel> fetchQuestionArticles(
+    int pageNum,
+    int pageSize, {
+    CancelToken? cancelToken,
+  }) async {
+    final Response<Map<String, dynamic>> response =
+        await HttpUtils.get<Map<String, dynamic>>(
+      API.qa(
+        pageNum: pageNum,
+      ),
+      queryParameters: <String, dynamic>{'page_size': pageSize},
+      cancelToken: cancelToken,
+    );
+    return RefreshArticleListModel.fromJson(response.data!);
+  }
+
+  static Future<List<ProjectTypeModel>> fetchProjectTypes({
+    CancelToken? cancelToken,
+  }) async {
+    final Response<List<dynamic>> response = await HttpUtils.get<List<dynamic>>(
+      API.projectType,
+      cancelToken: cancelToken,
+    );
+
+    return response.data!
+        .map(
+            (dynamic e) => ProjectTypeModel.fromJson(e as Map<String, dynamic>))
         .toList();
   }
 }
