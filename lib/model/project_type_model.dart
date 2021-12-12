cb5b30cae6c420e1bff5c69da70040144654055d
moxiaov587 <zouj1awang@163.com>
2021-12-12 17:22:40 +0800

    :hammer:add project type model

---
+++ lib/model/project_type_model.dart
@@ -0,0 +1,17 @@
+part of 'models.dart';
+
+@freezed
+class ProjectTypeModel with _$ProjectTypeModel {
+  factory ProjectTypeModel({
+    @Default(<ProjectTypeModel>[]) List<ProjectTypeModel> children,
+    @Default(0) int courseId,
+    @Default(0) int id,
+    @Default('') String name,
+    @Default(0) int order,
+    @Default(0) int parentChapterId,
+    @Default(0) int visible,
+  }) = _ProjectTypeModel;
+
+  factory ProjectTypeModel.fromJson(Map<String, dynamic> json) =>
+      _$ProjectTypeModelFromJson(json);
+}
