4235357a1e7089c08e35421acecb0322c68a9ee2
moxiaov587 <zouj1awang@163.com>
2021-12-19 14:31:13 +0800

    :hammer:add project type page in navigator

--- lib/screen/home/project.dart
+++ lib/screen/home/project.dart
@@ -36,19 +36,21 @@ class _ProjectState extends State<_Project> with AutomaticKeepAliveClientMixin {
         );
       },
       slivers: <Widget>[
-        SliverPersistentHeader(
-          pinned: true,
-          delegate: _ProjectTypeSwitchDelegate(),
+        SliverPinnedPersistentHeader(
+          delegate: _ProjectTypeSwitchSliverPinnedPersistentHeaderDelegate(
+            extentProtoType: const _ProjectTypeSwitchExtentProtoType(),
+          ),
         ),
       ],
     );
   }
 }
 
-class _ProjectTypeSwitchDelegate extends SliverPersistentHeaderDelegate {
+class _ProjectTypeSwitchExtentProtoType extends StatelessWidget {
+  const _ProjectTypeSwitchExtentProtoType({Key? key}) : super(key: key);
+
   @override
-  Widget build(
-      BuildContext context, double shrinkOffset, bool overlapsContent) {
+  Widget build(BuildContext context) {
     return Material(
       color: currentTheme.scaffoldBackgroundColor,
       child: Row(
@@ -69,15 +71,31 @@ class _ProjectTypeSwitchDelegate extends SliverPersistentHeaderDelegate {
       ),
     );
   }
+}
+
+class _ProjectTypeSwitchSliverPinnedPersistentHeaderDelegate
+    extends SliverPinnedPersistentHeaderDelegate {
+  _ProjectTypeSwitchSliverPinnedPersistentHeaderDelegate({
+    required Widget extentProtoType,
+  }) : super(
+          minExtentProtoType: extentProtoType,
+          maxExtentProtoType: extentProtoType,
+        );
 
   @override
-  double get maxExtent => 57.0;
+  Widget build(
+    BuildContext context,
+    double shrinkOffset,
+    double? minExtent,
+    double maxExtent,
+    bool overlapsContent,
+  ) {
+    return minExtentProtoType;
+  }
 
   @override
-  double get minExtent => 57.0;
-
-  @override
-  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) =>
+  bool shouldRebuild(
+          covariant SliverPinnedPersistentHeaderDelegate oldDelegate) =>
       false;
 }
 
@@ -86,138 +104,81 @@ class _ProjectTypeSwitch extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final ViewState<ProjectTypeModel> state =
-        ref.watch(currentProjectTypeProvider);
-
-    return state.when(
-      (ProjectTypeModel? value) => _ProjectTypeSwitchContainer(
-        onTap: () {
-          showModalBottomSheet<void>(
-            backgroundColor: Colors.transparent,
-            context: context,
-            builder: (_) => const _ProjectTypeBottomSheet(),
-          );
-        },
-        child: Text(value!.name),
-      ),
-      loading: () => const _ProjectTypeSwitchContainer(
-        child: LoadingWidget(
-          radius: 5.0,
-        ),
-      ),
-      error: (_, __, ___) => _ProjectTypeSwitchContainer(
-        onTap: () {
-          ref.read(projectTypesProvider.notifier).refresh();
-        },
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: const <Widget>[
-            Icon(Icons.refresh),
-            SizedBox(
-              width: 5,
-            ),
-            Text('refresh'),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class _ProjectTypeSwitchContainer extends StatelessWidget {
-  const _ProjectTypeSwitchContainer({
-    Key? key,
-    required this.child,
-    this.onTap,
-  }) : super(key: key);
-
-  final Widget child;
-  final VoidCallback? onTap;
-
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      borderRadius: BorderRadius.circular(20),
-      child: Container(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 12.0,
-          vertical: 8.0,
-        ),
-        child: child,
-      ),
-      onTap: onTap,
-    );
-  }
-}
-
-class _ProjectTypeBottomSheet extends StatefulWidget {
-  const _ProjectTypeBottomSheet({Key? key}) : super(key: key);
-
-  @override
-  __ProjectTypeBottomSheetState createState() =>
-      __ProjectTypeBottomSheetState();
-}
-
-class __ProjectTypeBottomSheetState extends State<_ProjectTypeBottomSheet>
-    with SingleTickerProviderStateMixin {
-  late final AnimationController _animationController = AnimationController(
-    duration: const Duration(seconds: 5),
-    vsync: this,
-  );
-
-  @override
-  void dispose() {
-    _animationController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return BottomSheet(
-      animationController: _animationController,
-      clipBehavior: Clip.antiAlias,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(20),
-      ),
-      onClosing: () {},
-      builder: (_) {
-        return SafeArea(
-          child: ListViewWidget<
-              StateNotifierProvider<ProjectTypeNotifier,
-                  ListViewState<ProjectTypeModel>>,
-              ProjectTypeModel>(
-            enablePullDown: true,
-            provider: projectTypesProvider,
-            builder: (_, __, List<ProjectTypeModel> list) {
-              return SliverList(
-                delegate: SliverChildBuilderDelegate(
-                  (_, int index) {
-                    return Consumer(
-                      builder: (_, WidgetRef ref, __) {
-                        final bool selected =
-                            ref.watch(projectTypeIsSelectedProvider(index));
-
-                        return ListTile(
-                          selected: selected,
-                          title: Text(list[index].name),
-                          onTap: () {
-                            ref
-                                .read(projectTypesProvider.notifier)
-                                .selected(index);
-
-                            Beamer.of(context).popRoute();
-                          },
-                        );
-                      },
-                    );
-                  },
-                  childCount: list.length,
-                ),
-              );
+    return ref.watch(currentProjectTypeProvider).when(
+          (ProjectTypeModel? value) => CapsuleInk(
+            onTap: () {
+              Beamer.of(context).beamToNamed(RouterName.projectType.location);
             },
+            child: Text(value!.name),
+          ),
+          loading: () => const CapsuleInk(
+            child: LoadingWidget(
+              radius: 5.0,
+            ),
+          ),
+          error: (_, __, ___) => CapsuleInk(
+            onTap: () {
+              ref.read(projectTypesProvider.notifier).refresh();
+            },
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                const Icon(Icons.refresh),
+                Gap(
+                  direction: GapDirection.horizontal,
+                  size: GapSize.small,
+                ),
+                Text('refresh'),
+              ],
+            ),
           ),
         );
-      },
+  }
+}
+
+class ProjectTypeBottomSheet extends StatelessWidget {
+  const ProjectTypeBottomSheet({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Material(
+        color: currentTheme.cardColor,
+        child: ListViewWidget<
+            StateNotifierProvider<ProjectTypeNotifier,
+                ListViewState<ProjectTypeModel>>,
+            ProjectTypeModel>(
+          enablePullDown: true,
+          provider: projectTypesProvider,
+          builder: (_, __, List<ProjectTypeModel> list) {
+            return SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (_, int index) {
+                  return Consumer(
+                    builder: (_, WidgetRef ref, __) {
+                      final bool selected =
+                          ref.watch(projectTypeIsSelectedProvider(index));
+
+                      return ListTile(
+                        selected: selected,
+                        title: Text(list[index].name),
+                        onTap: () {
+                          ref
+                              .read(projectTypesProvider.notifier)
+                              .selected(index);
+
+                          Beamer.of(context).popRoute();
+                        },
+                      );
+                    },
+                  );
+                },
+                childCount: list.length,
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
