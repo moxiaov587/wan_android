3b4bb11d476a3d53b15bd61a73120150354de2b0
moxiaov587 <zouj1awang@163.com>
2022-01-08 17:21:05 +0800

    :bento:Add icons for `pull_to_refresh_widgets`, and add some common icons

--- lib/contacts/icon_font_icons.dart
+++ lib/contacts/icon_font_icons.dart
@@ -24,4 +24,19 @@ class IconFontIcons {
   static const IconData lockLine = IconData(0xe85f, fontFamily: fontFamily);
   static const IconData lockFill = IconData(0xe85e, fontFamily: fontFamily);
   static const IconData restartLine = IconData(0xe74a, fontFamily: fontFamily);
+  static const IconData starLine = IconData(0xe87a, fontFamily: fontFamily);
+  static const IconData starFill = IconData(0xe879, fontFamily: fontFamily);
+  static const IconData refreshLine = IconData(0xe86c, fontFamily: fontFamily);
+  static const IconData errorWarningLine =
+      IconData(0xe853, fontFamily: fontFamily);
+  static const IconData checkLine = IconData(0xe847, fontFamily: fontFamily);
+  static const IconData arrowUpLine = IconData(0xe846, fontFamily: fontFamily);
+  static const IconData arrowDownLine =
+      IconData(0xe83d, fontFamily: fontFamily);
+  static const IconData shutDownLine = IconData(0xe750, fontFamily: fontFamily);
+  static const IconData globalLine = IconData(0xe6d3, fontFamily: fontFamily);
+  static const IconData deleteLine = IconData(0xe84e, fontFamily: fontFamily);
+  static const IconData userFill = IconData(0xe887, fontFamily: fontFamily);
+  static const IconData closeCircleLine =
+      IconData(0xe84a, fontFamily: fontFamily);
 }
