1b8e82f6baa1ad618f6f1a5133b573ef2fa2579b
moxiaov587 <zouj1awang@163.com>
2022-01-08 17:48:03 +0800

    :sparkles:Add `SliverChildBuilderDelegate.separated`

---
+++ lib/widget/custom_sliver_child_builder_delegate.dart
@@ -0,0 +1,46 @@
+import 'dart:math' as math;
+
+import 'package:flutter/material.dart';
+
+int _kDefaultSemanticIndexCallback(Widget _, int localIndex) => localIndex;
+
+class CustomSliverChildBuilderDelegate {
+  CustomSliverChildBuilderDelegate._();
+
+  static SliverChildBuilderDelegate separated({
+    required Widget Function(BuildContext, int) itemBuilder,
+    Widget Function(BuildContext, int)? separatorBuilder,
+    required int itemCount,
+    ChildIndexGetter? findChildIndexCallback,
+    bool addAutomaticKeepAlives = true,
+    bool addRepaintBoundaries = true,
+    bool addSemanticIndexes = true,
+    SemanticIndexCallback semanticIndexCallback =
+        _kDefaultSemanticIndexCallback,
+    int semanticIndexOffset = 0,
+  }) {
+    return SliverChildBuilderDelegate(
+      (BuildContext context, int index) {
+        final int itemIndex = index ~/ 2;
+        final Widget widget;
+        if (index.isEven) {
+          widget = itemBuilder(context, itemIndex);
+        } else {
+          widget =
+              separatorBuilder?.call(context, itemIndex) ?? const Divider();
+        }
+        return widget;
+      },
+      childCount: math.max(
+        0,
+        itemCount * 2 - 1,
+      ),
+      findChildIndexCallback: findChildIndexCallback,
+      addAutomaticKeepAlives: addAutomaticKeepAlives,
+      addRepaintBoundaries: addRepaintBoundaries,
+      addSemanticIndexes: addSemanticIndexes,
+      semanticIndexCallback: semanticIndexCallback,
+      semanticIndexOffset: semanticIndexOffset,
+    );
+  }
+}
