3e07fe1cfbea88af0ef57afb7780524881ad751f
moxiaov587 <zouj1awang@163.com>
2022-01-16 16:17:51 +0800

    :zap:Use a smaller refresh range to disable pull-up and pull-down and use a new way to set padding bottom

--- lib/app/provider/widget/refresh_list_view_widget.dart
+++ lib/app/provider/widget/refresh_list_view_widget.dart
@@ -18,6 +18,7 @@ class RefreshListViewWidget<
     this.onInitState,
     required this.builder,
     this.slivers,
+    this.paddingBottom,
   }) : super(key: key);
 
   final T provider;
@@ -25,6 +26,7 @@ class RefreshListViewWidget<
   final RefreshListViewBuilder<S> builder;
 
   final List<Widget>? slivers;
+  final double? paddingBottom;
 
   @override
   _RefreshListViewWidgetState<T, S> createState() =>
@@ -52,48 +54,34 @@ class _RefreshListViewWidgetState<
 
   @override
   Widget build(BuildContext context) {
-    /// if [RefreshListViewState] is [RefreshListViewStateLoading],
-    /// disabled enablePullDown
-    final bool enablePullDown = ref.watch(
-      widget.provider.select((RefreshListViewState<S> value) =>
-          value.whenOrNull(
-            (_, __, ___) => true,
-            error: (_, __, ___) => true,
-          ) ??
-          false),
-    );
-
-    /// if [RefreshListViewState] is [RefreshListViewStateLoading] or
-    /// [RefreshListViewStateError], disabled enablePullUp
-    final bool enablePullUp = ref.watch(
-      widget.provider.select((RefreshListViewState<S> value) =>
-          value.whenOrNull((_, __, ___) => true) ?? false),
-    );
-
-    return SmartRefresher(
-      enablePullDown: enablePullDown,
-      enablePullUp: enablePullUp,
-      header: const DropDownListHeader(),
-      footer: const LoadMoreListFooter(),
+    return SmartRefresher.builder(
+      enablePullDown: true,
+      enablePullUp: true,
       controller: _refreshController,
       onRefresh: () async {
+        if (_refreshController.footerMode?.value == LoadStatus.noMore) {
+          _refreshController.resetNoData();
+        }
         final RefreshControllerStatus status =
             await ref.read(widget.provider.notifier).refresh();
+
         switch (status) {
           case RefreshControllerStatus.completed:
+            _refreshController.refreshCompleted();
+            break;
+          case RefreshControllerStatus.noData:
+            _refreshController.loadNoData();
             _refreshController.refreshCompleted();
             break;
           case RefreshControllerStatus.failed:
             _refreshController.refreshFailed();
             break;
-          default:
-
-          /// [status] is null
         }
       },
       onLoading: () async {
         final RefreshControllerStatus? status =
             await ref.read(widget.provider.notifier).loadMore();
+
         switch (status) {
           case RefreshControllerStatus.completed:
             _refreshController.loadComplete();
@@ -105,55 +93,66 @@ class _RefreshListViewWidgetState<
             _refreshController.loadFailed();
             break;
           default:
-
-          /// [status] is null
+            break;
         }
       },
-      child: CustomScrollView(
-        slivers: <Widget>[
-          if (widget.slivers != null && widget.slivers!.isNotEmpty)
-            ...widget.slivers!,
-          Consumer(
-            builder: (BuildContext context, WidgetRef ref, Widget? child) {
-              return ref.watch(widget.provider).when(
-                    (int nextPageNum, bool isLastPage, List<S> list) =>
-                        list.isEmpty
-                            ? const EmptyWidget()
-                            : widget.builder(context, ref, list),
-                    loading: () => const SliverFillRemaining(
-                      child: LoadingWidget(),
+      builder: (_, RefreshPhysics physics) {
+        return CustomScrollView(
+          physics: physics,
+          slivers: <Widget>[
+            /// if [RefreshListViewState] is [RefreshListViewStateLoading],
+            /// disabled enablePullDown
+            ref.watch(
+              widget.provider.select(
+                (RefreshListViewState<S> value) =>
+                    value.whenOrNull(
+                      (_, __, ___) => const DropDownListHeader(),
+                      error: (_, __, ___) => const DropDownListHeader(),
+                    ) ??
+                    const SliverToBoxAdapter(),
+              ),
+            ),
+            if (widget.slivers != null && widget.slivers!.isNotEmpty)
+              ...widget.slivers!,
+            ref.watch(widget.provider).when(
+                  (int nextPageNum, bool isLastPage, List<S> list) =>
+                      list.isEmpty
+                          ? const EmptyWidget()
+                          : widget.builder(context, ref, list),
+                  loading: () => const SliverFillRemaining(
+                    child: LoadingWidget(),
+                  ),
+                  error: (int? statusCode, String? message, String? detail) =>
+                      SliverFillRemaining(
+                    child: CustomErrorWidget(
+                      statusCode: statusCode,
+                      message: message,
+                      detail: detail,
+                      onRetry: _refreshController.requestRefresh,
                     ),
-                    error: (int? statusCode, String? message, String? detail) =>
-                        SliverFillRemaining(
-                      child: CustomErrorWidget(
-                        statusCode: statusCode,
-                        message: message,
-                        detail: detail,
-                        onRetry: () async {
-                          _refreshController.requestRefresh();
+                  ),
+                ),
 
-                          final RefreshControllerStatus status = await ref
-                              .read(widget.provider.notifier)
-                              .refresh();
-                          switch (status) {
-                            case RefreshControllerStatus.completed:
-                              _refreshController.refreshCompleted();
-                              break;
-                            case RefreshControllerStatus.failed:
-                              _refreshController.refreshFailed();
-                              break;
-                            default:
-
-                            /// [status] is null
-                          }
-                        },
-                      ),
-                    ),
-                  );
-            },
-          )
-        ],
-      ),
+            /// if [RefreshListViewState] is [RefreshListViewStateLoading] or
+            /// [RefreshListViewStateError], disabled enablePullUp
+            ref.watch(
+              widget.provider.select(
+                (RefreshListViewState<S> value) =>
+                    value.whenOrNull(
+                        (_, __, ___) => const LoadMoreListFooter()) ??
+                    const SliverToBoxAdapter(),
+              ),
+            ),
+            SliverToBoxAdapter(
+              child: widget.paddingBottom != null
+                  ? SizedBox(
+                      height: widget.paddingBottom,
+                    )
+                  : null,
+            ),
+          ],
+        );
+      },
     );
   }
 }
@@ -168,6 +167,7 @@ class AutoDisposeRefreshListViewWidget<
     this.onInitState,
     required this.builder,
     this.slivers,
+    this.paddingBottom,
   }) : super(key: key);
 
   final T provider;
@@ -175,6 +175,7 @@ class AutoDisposeRefreshListViewWidget<
   final RefreshListViewBuilder<S> builder;
 
   final List<Widget>? slivers;
+  final double? paddingBottom;
 
   @override
   _AutoDisposeRefreshListViewWidgetState<T, S> createState() =>
@@ -202,48 +203,34 @@ class _AutoDisposeRefreshListViewWidgetState<
 
   @override
   Widget build(BuildContext context) {
-    /// if [RefreshListViewState] is [RefreshListViewStateLoading],
-    /// disabled enablePullDown
-    final bool enablePullDown = ref.watch(
-      widget.provider.select((RefreshListViewState<S> value) =>
-          value.whenOrNull(
-            (_, __, ___) => true,
-            error: (_, __, ___) => true,
-          ) ??
-          false),
-    );
-
-    /// if [RefreshListViewState] is [RefreshListViewStateLoading] or
-    /// [RefreshListViewStateError], disabled enablePullUp
-    final bool enablePullUp = ref.watch(
-      widget.provider.select((RefreshListViewState<S> value) =>
-          value.whenOrNull((_, __, ___) => true) ?? false),
-    );
-
-    return SmartRefresher(
-      enablePullDown: enablePullDown,
-      enablePullUp: enablePullUp,
-      header: const DropDownListHeader(),
-      footer: const LoadMoreListFooter(),
+    return SmartRefresher.builder(
+      enablePullDown: true,
+      enablePullUp: true,
       controller: _refreshController,
       onRefresh: () async {
+        if (_refreshController.footerMode?.value == LoadStatus.noMore) {
+          _refreshController.resetNoData();
+        }
         final RefreshControllerStatus status =
             await ref.read(widget.provider.notifier).refresh();
+
         switch (status) {
           case RefreshControllerStatus.completed:
+            _refreshController.refreshCompleted();
+            break;
+          case RefreshControllerStatus.noData:
+            _refreshController.loadNoData();
             _refreshController.refreshCompleted();
             break;
           case RefreshControllerStatus.failed:
             _refreshController.refreshFailed();
             break;
-          default:
-
-          /// [status] is null
         }
       },
       onLoading: () async {
         final RefreshControllerStatus? status =
             await ref.read(widget.provider.notifier).loadMore();
+
         switch (status) {
           case RefreshControllerStatus.completed:
             _refreshController.loadComplete();
@@ -255,55 +242,66 @@ class _AutoDisposeRefreshListViewWidgetState<
             _refreshController.loadFailed();
             break;
           default:
-
-          /// [status] is null
+            break;
         }
       },
-      child: CustomScrollView(
-        slivers: <Widget>[
-          if (widget.slivers != null && widget.slivers!.isNotEmpty)
-            ...widget.slivers!,
-          Consumer(
-            builder: (BuildContext context, WidgetRef ref, Widget? child) {
-              return ref.watch(widget.provider).when(
-                    (int nextPageNum, bool isLastPage, List<S> list) =>
-                        list.isEmpty
-                            ? const EmptyWidget()
-                            : widget.builder(context, ref, list),
-                    loading: () => const SliverFillRemaining(
-                      child: LoadingWidget(),
+      builder: (_, RefreshPhysics physics) {
+        return CustomScrollView(
+          physics: physics,
+          slivers: <Widget>[
+            /// if [RefreshListViewState] is [RefreshListViewStateLoading],
+            /// disabled enablePullDown
+            ref.watch(
+              widget.provider.select(
+                (RefreshListViewState<S> value) =>
+                    value.whenOrNull(
+                      (_, __, ___) => const DropDownListHeader(),
+                      error: (_, __, ___) => const DropDownListHeader(),
+                    ) ??
+                    const SliverToBoxAdapter(),
+              ),
+            ),
+            if (widget.slivers != null && widget.slivers!.isNotEmpty)
+              ...widget.slivers!,
+            ref.watch(widget.provider).when(
+                  (int nextPageNum, bool isLastPage, List<S> list) =>
+                      list.isEmpty
+                          ? const EmptyWidget()
+                          : widget.builder(context, ref, list),
+                  loading: () => const SliverFillRemaining(
+                    child: LoadingWidget(),
+                  ),
+                  error: (int? statusCode, String? message, String? detail) =>
+                      SliverFillRemaining(
+                    child: CustomErrorWidget(
+                      statusCode: statusCode,
+                      message: message,
+                      detail: detail,
+                      onRetry: _refreshController.requestRefresh,
                     ),
-                    error: (int? statusCode, String? message, String? detail) =>
-                        SliverFillRemaining(
-                      child: CustomErrorWidget(
-                        statusCode: statusCode,
-                        message: message,
-                        detail: detail,
-                        onRetry: () async {
-                          _refreshController.requestRefresh();
+                  ),
+                ),
 
-                          final RefreshControllerStatus status = await ref
-                              .read(widget.provider.notifier)
-                              .refresh();
-                          switch (status) {
-                            case RefreshControllerStatus.completed:
-                              _refreshController.refreshCompleted();
-                              break;
-                            case RefreshControllerStatus.failed:
-                              _refreshController.refreshFailed();
-                              break;
-                            default:
-
-                            /// [status] is null
-                          }
-                        },
-                      ),
-                    ),
-                  );
-            },
-          )
-        ],
-      ),
+            /// if [RefreshListViewState] is [RefreshListViewStateLoading] or
+            /// [RefreshListViewStateError], disabled enablePullUp
+            ref.watch(
+              widget.provider.select(
+                (RefreshListViewState<S> value) =>
+                    value.whenOrNull(
+                        (_, __, ___) => const LoadMoreListFooter()) ??
+                    const SliverToBoxAdapter(),
+              ),
+            ),
+            SliverToBoxAdapter(
+              child: widget.paddingBottom != null
+                  ? SizedBox(
+                      height: widget.paddingBottom,
+                    )
+                  : null,
+            ),
+          ],
+        );
+      },
     );
   }
 }
