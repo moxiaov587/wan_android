f3e9efca2022665798c282a944e4c59d98645a74
moxiaov587 <zouj1awang@163.com>
2022-01-16 16:55:19 +0800

    :hammer:Add `drawer_provider` file

---
+++ lib/screen/drawer/provider/drawer_provider.dart
@@ -0,0 +1,10 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../app/http/wan_android_api.dart';
+import '../../../app/provider/provider.dart';
+import '../../../app/provider/view_state.dart';
+import '../../../model/models.dart';
+
+part 'my_points_provider.dart';
+part 'points_rank_provider.dart';
