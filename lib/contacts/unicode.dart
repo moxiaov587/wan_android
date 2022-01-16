79b0077b793892d5864b797baef8cb21d7316771
moxiaov587 <zouj1awang@163.com>
2022-01-16 16:25:02 +0800

    :hammer:Add `unicode`

---
+++ lib/contacts/unicode.dart
@@ -0,0 +1,8 @@
+class Unicode {
+  Unicode._();
+
+  static const String fullWidthSpace = '\u{3000}';
+  static const String halfWidthSpace = '\u{0020}';
+  static const String notBreakSpace = '\u{00A0}';
+  static const String zeroWidthSpace = '\u{200B}';
+}
