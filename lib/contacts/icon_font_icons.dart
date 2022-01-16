7aa2c8ca11aa287ee5d7c0380f9870f1aba58539
moxiaov587 <zouj1awang@163.com>
2022-01-16 15:50:58 +0800

    :bento:Add some icons

--- lib/contacts/icon_font_icons.dart
+++ lib/contacts/icon_font_icons.dart
@@ -3,40 +3,48 @@ import 'package:flutter/widgets.dart' show IconData;
 class IconFontIcons {
   static const String fontFamily = 'IconFont';
 
-  static const IconData seedlingLine = IconData(0xe834, fontFamily: fontFamily);
-  static const IconData seedlingFill = IconData(0xe833, fontFamily: fontFamily);
-  static const IconData androidLine = IconData(0xe7c8, fontFamily: fontFamily);
-  static const IconData androidFill = IconData(0xe7c7, fontFamily: fontFamily);
-  static const IconData questionnaireLine =
-      IconData(0xe6ff, fontFamily: fontFamily);
-  static const IconData questionnaireFill =
-      IconData(0xe6fd, fontFamily: fontFamily);
-  static const IconData homeFill = IconData(0xe6b9, fontFamily: fontFamily);
-  static const IconData homeLine = IconData(0xe6b2, fontFamily: fontFamily);
-  static const IconData searchEyeLine =
-      IconData(0xe86e, fontFamily: fontFamily);
-  static const IconData searchEyeFill =
-      IconData(0xe86d, fontFamily: fontFamily);
-  static const IconData accountCircleLine =
-      IconData(0xe87f, fontFamily: fontFamily);
-  static const IconData accountCircleFill =
-      IconData(0xe87e, fontFamily: fontFamily);
-  static const IconData lockLine = IconData(0xe85f, fontFamily: fontFamily);
-  static const IconData lockFill = IconData(0xe85e, fontFamily: fontFamily);
-  static const IconData restartLine = IconData(0xe74a, fontFamily: fontFamily);
-  static const IconData starLine = IconData(0xe87a, fontFamily: fontFamily);
-  static const IconData starFill = IconData(0xe879, fontFamily: fontFamily);
-  static const IconData refreshLine = IconData(0xe86c, fontFamily: fontFamily);
-  static const IconData errorWarningLine =
-      IconData(0xe853, fontFamily: fontFamily);
-  static const IconData checkLine = IconData(0xe847, fontFamily: fontFamily);
-  static const IconData arrowUpLine = IconData(0xe846, fontFamily: fontFamily);
-  static const IconData arrowDownLine =
-      IconData(0xe83d, fontFamily: fontFamily);
-  static const IconData shutDownLine = IconData(0xe750, fontFamily: fontFamily);
-  static const IconData globalLine = IconData(0xe6d3, fontFamily: fontFamily);
-  static const IconData deleteLine = IconData(0xe84e, fontFamily: fontFamily);
-  static const IconData userFill = IconData(0xe887, fontFamily: fontFamily);
   static const IconData closeCircleLine =
-      IconData(0xe84a, fontFamily: fontFamily);
+      IconData(0xeb97, fontFamily: fontFamily);
+  static const IconData userFill = IconData(0xf253, fontFamily: fontFamily);
+  static const IconData deleteLine = IconData(0xec24, fontFamily: fontFamily);
+  static const IconData globalLine = IconData(0xedcf, fontFamily: fontFamily);
+  static const IconData shutDownLine = IconData(0xf126, fontFamily: fontFamily);
+  static const IconData arrowDownLine =
+      IconData(0xea4c, fontFamily: fontFamily);
+  static const IconData arrowUpLine = IconData(0xea76, fontFamily: fontFamily);
+  static const IconData checkLine = IconData(0xeb7b, fontFamily: fontFamily);
+  static const IconData errorWarningLine =
+      IconData(0xeca1, fontFamily: fontFamily);
+  static const IconData restartLine = IconData(0xf080, fontFamily: fontFamily);
+  static const IconData refreshLine = IconData(0xf064, fontFamily: fontFamily);
+  static const IconData starFill = IconData(0xf186, fontFamily: fontFamily);
+  static const IconData starLine = IconData(0xf18b, fontFamily: fontFamily);
+  static const IconData lockLine = IconData(0xeecc, fontFamily: fontFamily);
+  static const IconData lockFill = IconData(0xeecb, fontFamily: fontFamily);
+  static const IconData accountCircleLine =
+      IconData(0xea09, fontFamily: fontFamily);
+  static const IconData accountCircleFill =
+      IconData(0xea08, fontFamily: fontFamily);
+  static const IconData searchEyeLine =
+      IconData(0xf0cf, fontFamily: fontFamily);
+  static const IconData searchLine = IconData(0xf0cd, fontFamily: fontFamily);
+  static const IconData homeLine = IconData(0xee19, fontFamily: fontFamily);
+  static const IconData homeFill = IconData(0xee2e, fontFamily: fontFamily);
+  static const IconData questionnaireLine =
+      IconData(0xf048, fontFamily: fontFamily);
+  static const IconData questionnaireFill =
+      IconData(0xf047, fontFamily: fontFamily);
+  static const IconData androidLine = IconData(0xea36, fontFamily: fontFamily);
+  static const IconData androidFill = IconData(0xea35, fontFamily: fontFamily);
+  static const IconData seedlingLine = IconData(0xf0d5, fontFamily: fontFamily);
+  static const IconData seedlingFill = IconData(0xf0d4, fontFamily: fontFamily);
+  static const IconData shareCircleLine =
+      IconData(0xf0f6, fontFamily: fontFamily);
+  static const IconData coinLine = IconData(0xebb2, fontFamily: fontFamily);
+  static const IconData settingsLine = IconData(0xf0e6, fontFamily: fontFamily);
+  static const IconData honourLine = IconData(0xee35, fontFamily: fontFamily);
+  static const IconData informationLine =
+      IconData(0xee59, fontFamily: fontFamily);
+  static const IconData sunFill = IconData(0xf1bc, fontFamily: fontFamily);
+  static const IconData moonLine = IconData(0xef75, fontFamily: fontFamily);
 }
