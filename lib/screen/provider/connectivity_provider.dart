0930b901f1467614d7525476369343ce03048c53
moxiaov587 <zouj1awang@163.com>
2022-02-06 16:41:21 +0800

    :hammer:Add tips in mobile network environment

--- lib/screen/provider/connectivity_provider.dart
+++ lib/screen/provider/connectivity_provider.dart
@@ -2,8 +2,10 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/services.dart' show PlatformException;
 import 'package:riverpod/riverpod.dart';
 
+import '../../app/l10n/generated/l10n.dart';
 import '../../app/provider/provider.dart';
 import '../../app/provider/view_state.dart';
+import '../../utils/dialog.dart';
 
 final StateNotifierProvider<ConnectivityNotifier, ViewState<ConnectivityResult>>
     connectivityProvider =
@@ -36,5 +38,9 @@ class ConnectivityNotifier extends BaseViewNotifier<ConnectivityResult> {
 
   Future<void> updateData(ConnectivityResult result) async {
     state = ViewStateData<ConnectivityResult>(value: result);
+
+    if (result == ConnectivityResult.mobile) {
+      DialogUtils.tips(S.current.useDataTrafficTips);
+    }
   }
 }
