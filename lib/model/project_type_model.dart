0a8eb685a590dab0210d1a506fa2b7527f78a350
moxiaov587 <zouj1awang@163.com>
2022-06-18 19:44:02 +0800

    :hammer: Remove isSelected

--- lib/model/project_type_model.dart
+++ lib/model/project_type_model.dart
@@ -3,14 +3,13 @@ part of 'models.dart';
 @freezed
 class ProjectTypeModel with _$ProjectTypeModel {
   factory ProjectTypeModel({
-    @Default(<ProjectTypeModel>[]) List<ProjectTypeModel> children,
+    @Default(<ProjectTypeModel>[]) final List<ProjectTypeModel> children,
     @Default(0) int courseId,
     @Default(0) int id,
     @Default('') String name,
     @Default(0) int order,
     @Default(0) int parentChapterId,
     @Default(0) int visible,
-    @JsonKey(ignore: true) @Default(false) bool isSelected,
   }) = _ProjectTypeModel;
 
   factory ProjectTypeModel.fromJson(Map<String, dynamic> json) =>
